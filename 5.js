9370179a224897e50eedd28af5ae76fc7bacb82c
KonTroAl <troshenkin.k@yandex.ru>
2020-11-23 20:51:51 +0500

    task 5 completed

---
+++ 5.js
@@ -0,0 +1,25 @@
+// 5. Реализовать четыре основные арифметические операции в виде функций с двумя параметрами. Обязательно использовать оператор return.
+
+let a = 5
+let b = 2
+
+function my_sum(a, b) {
+    return a + b
+}
+
+function my_sub(a, b) {
+    return a - b
+}
+
+function my_mul(a, b) {
+    return a * b
+}
+
+function my_div(a, b) {
+    return a / b
+}
+
+document.writeln(my_sum(a, b))
+document.writeln(my_sub(a, b))
+document.writeln(my_mul(a, b))
+document.writeln(my_div(a, b))
